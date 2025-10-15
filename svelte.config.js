b6623cc8df29c87e22446279c02bd553c37c5a3d
anderwya000 <155918698+anderwya000@users.noreply.github.com>
2025-10-15 13:05:17 +0000

    fix: maybe maybe this tiime

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,6 @@ const config = {
         }),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/schedule' : '',
-            assets: process.env.NODE_ENV === 'production' ? '/schedule' : '',
         },
     }
 };
