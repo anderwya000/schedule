3040e2d59caf90dcf266576d58e43f2904184f0b
anderwya000 <155918698+anderwya000@users.noreply.github.com>
2025-10-15 20:17:33 +0000

    feat: add settings menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
-<link href="https://fonts.googleapis.com/css2?family=PT+Mono" rel="stylesheet">
 <script>
-  import Clock from "$lib/components/Clock.svelte";
-  import ScheduleList from "$lib/components/ScheduleList.svelte";
-  import RemainingTime from "$lib/components/RemainingTime.svelte";
-  import sched_normal from "$lib/data/normal.json";
-  import sched_sst from "$lib/data/sst.json";
-  import dayjs from "dayjs";
+    import Clock from "$lib/components/Clock.svelte";
+    import ScheduleList from "$lib/components/ScheduleList.svelte";
+    import RemainingTime from "$lib/components/RemainingTime.svelte";
+    import sched_normal from "$lib/data/normal.json";
+    import sched_sst from "$lib/data/sst.json";
+    import dayjs from "dayjs";
+    import Settings from "$lib/components/Settings.svelte";
 
-  const schedule = dayjs().day() === 2 ? sched_sst : sched_normal;
+    const schedule = dayjs().day() === 2 ? sched_sst : sched_normal;
 </script>
+
+<link
+    href="https://fonts.googleapis.com/css2?family=PT+Mono"
+    rel="stylesheet"
+/>
 
 <Clock />
 <RemainingTime {schedule} />
 <ScheduleList {schedule} />
+<Settings />
